bc6721fae3f616bcb3ac4cf1e668b73d292308f6
Lepecin <evantrulife@gmail.com>
2024-03-31 23:28:06 +0300

    Create lightning model

---
+++ src/geoanalytics/model/interface.py
@@ -0,0 +1,34 @@
+import torch
+from lightning import LightningModule
+
+from .bulktrans import BulkTransformerModel, BulkTransformerConfig
+
+
+class BulkTransformerInterface(LightningModule):
+    def __init__(self, config: BulkTransformerConfig) -> None:
+        super().__init__()
+
+        self.model = BulkTransformerModel(config)
+
+    def forward(self, input: dict[str, torch.Tensor]) -> torch.Tensor:
+        return self.model.forward(input)
+
+    @staticmethod
+    def loss_function(output: torch.Tensor, target: torch.Tensor, *, eps: float = 1e-8):
+        return (
+            torch.log(torch.clip(output, eps, 1 - eps))
+            .mul(target)
+            .add(torch.log(torch.clip(1 - output, eps, 1 - eps)).mul(1 - target))
+            .neg()
+            .sum(1)
+            .mean()
+        )
+
+    def training_step(self, batch: dict[str, torch.Tensor]) -> torch.Tensor:
+        loss = self.loss_function(self.forward(batch), batch["target"])
+        self.log("train_loss", loss.detach().item())
+        return loss
+
+    def validation_step(self, batch: dict[str, torch.Tensor]):
+        loss = self.loss_function(self.forward(batch), batch["target"])
+        self.log("val_loss", loss.detach().item())
