b2e241039c64ae33e7f2c473b6cdb7728bb4e409
Lepecin <evantrulife@gmail.com>
2024-04-10 09:01:42 +0300

    Add config

---
+++ src/geoanalytics/data/config.py
@@ -0,0 +1,22 @@
+CONTINUOUS_FEATURES = [
+    "count",
+    "count_distinct",
+    "lat",
+    "lng",
+    "sum",
+    "avg",
+    "min",
+    "max",
+    "std",
+]
+CATEGORICAL_FEATURES = ["datetime_id", "mcc_code"]
+CATEGORICAL_SIZES = [4, 24]
+FRACTION = 0.8
+HIDDEN_SIZE = 16
+BATCH_SIZE = 64
+HEADS = 4
+LAYERS = 1
+FEEDFORWARD_SIZE = 64
+DROPOUT = 0.1
+LEARNING_RATE = 0.01
+EPOCHS = 1
