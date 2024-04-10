df5c8d01c1a52a05968e0e9c2102f8f375409276
Lepecin <evantrulife@gmail.com>
2024-04-10 09:01:20 +0300

    Finalise running script

--- run.py
+++ run.py
@@ -1,20 +1,19 @@
 import subprocess
 import pathlib
+import sys
 
 directory = pathlib.Path(__file__).parent
-python_path = directory / ".venv/bin/python"
-
 
 subprocess.run(
     [
-        str(python_path),
-        directory / "src/geoanalytics/project/app.py",
+        sys.executable,
+        directory / "src/geoanalytics/naive_baseline/submit/run.py",
         "-ht",
         directory / "data/geophysical/hexses_target.lst",
         "-hd",
         directory / "data/geophysical/hexses_data.lst",
         "-i",
-        directory / "data/geophysical/moscow.parquet",
+        directory / "data/bank/target.parquet",
         "-o",
         directory / "data/output.parquet",
     ]
